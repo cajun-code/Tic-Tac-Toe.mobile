ec40f862d1aee0ccbebd951ca85de3696c31e1a7
Yevhen Strohanov <ystrohanov@gmail.com>
2019-02-02 17:08:15 +0300

    minimax algo implementation

---
+++ tic-tac-toe/Models/BoardModel.swift
@@ -0,0 +1,9 @@
+//
+//  BoardModel.swift
+//  tic-tac-toe
+//
+//  Created by Yevhen Strohanov on 2/2/19.
+//  Copyright © 2019 Organization Name. All rights reserved.
+//
+
+import Foundation
---
+++ tic-tac-toe/Views/Board.swift
@@ -0,0 +1,9 @@
+//
+//  Board.swift
+//  tic-tac-toe
+//
+//  Created by Yevhen Strohanov on 2/2/19.
+//  Copyright © 2019 Organization Name. All rights reserved.
+//
+
+import Foundation
