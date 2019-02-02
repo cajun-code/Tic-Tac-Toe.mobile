f8153be63b08eeb3150156b7acef2b0e486a93f1
Yevhen Strohanov <ystrohanov@gmail.com>
2019-02-02 14:13:21 +0300

    Move sctruct added

---
+++ tic-tac-toe/Move.swift
@@ -0,0 +1,14 @@
+//
+//  Move.swift
+//  tic-tac-toe
+//
+//  Created by Yevhen Strohanov on 2/2/19.
+//  Copyright © 2019 Organization Name. All rights reserved.
+//
+
+import Foundation
+
+struct Move {
+    var row = -1
+    var col = -1
+}
