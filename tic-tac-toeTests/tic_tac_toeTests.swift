c722054110fee03844292c3ee3d05c4c839d49af
Yevhen Strohanov <ystrohanov@gmail.com>
2019-02-03 07:32:58 +0300

    add test on expected computer behavior

--- tic-tac-toeTests/tic_tac_toeTests.swift
+++ tic-tac-toeTests/tic_tac_toeTests.swift
@@ -11,24 +11,30 @@ import XCTest
 
 class tic_tac_toeTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testComputerTakesWinShot() {
+        var board = [
+            ["o", " ", "x"],
+            [" ", "x", " "],
+            ["o", " ", " "]
+        ]
+        let winMove = Opponent.shared.findBestMove(&board)
+        XCTAssert(winMove.row == 1)
+        XCTAssert(winMove.col == 0)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testComputerBlocksUsersWinShot() {
+        var board = [
+            ["o", " ", "x"],
+            [" ", "o", " "],
+            [" ", " ", "x"]
+        ]
+        let blockMove = Opponent.shared.findBestMove(&board)
+        XCTAssert(blockMove.row == 1)
+        XCTAssert(blockMove.col == 2)
     }
-
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
