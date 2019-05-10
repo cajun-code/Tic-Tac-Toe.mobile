53e51d82ec12fe599706459f8a8ef9d3060f3542
Parupally, SureshReddy (Accenture) <rsurf@allstate.com>
2019-05-10 09:26:29 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/tic_tac_toe_test_suresh/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tic_tac_toe_test_suresh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
