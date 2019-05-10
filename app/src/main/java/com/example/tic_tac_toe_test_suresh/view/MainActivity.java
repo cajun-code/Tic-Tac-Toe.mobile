f846c58329e0c8e1a4450cb85b27dd85492a62ae
Parupally, SureshReddy (Accenture) <rsurf@allstate.com>
2019-05-10 10:03:32 -0500

    Dependencies and UI added

--- app/src/main/java/com/example/tic_tac_toe_test_suresh/view/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe_test_suresh/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.tic_tac_toe_test_suresh;
+package com.example.tic_tac_toe_test_suresh.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.tic_tac_toe_test_suresh.R;
 
 public class MainActivity extends AppCompatActivity {
 
